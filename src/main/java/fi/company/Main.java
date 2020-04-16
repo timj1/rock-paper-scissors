1dd2ffbd936e75bc2c9d1e6018c3e1566827a729
Timo Jurvelin <tintze85@hotmail.com>
2020-04-16 16:33:50 +0300

    hei maailma

--- src/main/java/fi/company/Main.java
+++ src/main/java/fi/company/Main.java
@@ -3,6 +3,6 @@ package fi.company;
 
 public class Main {
     public static void main(String [] args) {
-        System.out.println("hello world");
+        System.out.println("hei maailma");
     }
 }
