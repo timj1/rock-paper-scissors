bb896ac545eb793afc1215da5ea34c4c6981487f
Timo Jurvelin <tintze85@hotmail.com>
2020-04-17 11:02:15 +0300

    game functions added

--- src/main/java/fi/company/Main.java
+++ src/main/java/fi/company/Main.java
@@ -1,8 +1,53 @@
 
 package fi.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String [] args) {
-        System.out.println("hei maailma");
+        Scanner input = new Scanner(System.in);
+
+        int pelaaja = 0;
+        int pelaajaAi = 0;
+        do {
+            System.out.println("Choose your weapon:");
+            String valinta = input.nextLine();
+
+            String tietokone = "";
+            int number = (int) (Math.random() * 3);
+            if(number < 1) {
+                tietokone = "rock";
+            } else if(number < 2) {
+                tietokone = "paper";
+            } else {
+                tietokone = "scissors";
+            }
+            System.out.println("Computer chose " + tietokone);
+            if(!valinta.equals(tietokone)) {
+                if(valinta.equals("rock") && tietokone.equals("paper")) {
+                    System.out.println("Computer won!");
+                    pelaajaAi++;
+                } else if (valinta.equals("paper") && tietokone.equals("scissors")) {
+                    System.out.println("Computer won!");
+                    pelaajaAi++;
+                } else if (valinta.equals("scissors") && tietokone.equals("rock")) {
+                    System.out.println("Computer won!");
+                    pelaajaAi++;
+                } else {
+                    System.out.println("You won!");
+                    pelaaja++;
+                }
+            } else {
+                System.out.println("Tie!");
+            }
+        } while(pelaaja != 3 && pelaajaAi !=3);
+
+        System.out.println("Pelaaja " + pelaaja + " tietokone " + pelaajaAi);
+        if(pelaaja < pelaajaAi) {
+            System.out.println("Computer won!");
+        } else {
+            System.out.println("You won!");
+        }
+        
     }
 }
